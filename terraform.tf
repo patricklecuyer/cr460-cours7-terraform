91d684406d961803e0dfb26b7a996ed05104628e
Patrick Lecuyer <patl@patl.io>
2018-02-26 16:46:06 -0800

    Initial commit

---
+++ terraform.tf
@@ -0,0 +1,50 @@
+resource "google_compute_instance" "instance1" {
+  name         = "instance1"                 # Nom
+  machine_type = "n1-standard-1"             # Taille de la machine
+  zone         = "northamerica-northeast1-a" # Zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8" # Disque
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.cr460-subnet1.self_link}" # Interface Reseau
+  }
+
+  tags = ["web", "patate", "cr460"]
+}
+
+#Definition du sous-reseau
+resource "google_compute_subnetwork" "cr460-subnet1" {
+  name          = "cr460-subnet1"                             # Nom
+  ip_cidr_range = "10.0.0.0/24"                               # Adresse IP
+  network       = "${google_compute_network.cr460.self_link}" # Liens vers le reseau
+  region        = "northamerica-northeast1"                   # Region
+}
+
+# Definition du VPC
+resource "google_compute_network" "cr460" {
+  name                    = "cr460" # Nom du reseau
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "http"
+  network = "${google_compute_network.cr460.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags = ["web"]
+}
+
+# Definir le fournisseur nuagique
+provider "google" {
+  credentials = "${file("account.json")}"
+  project     = "cr460-cours6"
+  region      = "northamerica-northeast1"
+}
